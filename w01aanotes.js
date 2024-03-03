73174abf6e33085ac13781e244273d824571d4c8
Juan-of-many <fiery.fynx@gmail.com>
2024-03-02 17:06:48 -0900

    javascript debugging practice

---
+++ w01aanotes.js
@@ -0,0 +1,13 @@
+//Write a JavaScript statement that declares and instantiates a variable that can be changed named "firstName" that holds the string "Antonia Francesca"
+let firstName = "Antonia Frnacesca";
+
+/*
+HTML: Markup language that we use to structure and give meaning to our web content, for example defining paragraphs, headings, and data tables, or embedding images and videos in the page. 
+CSS: Language of style rules that we use to apply styling to markup content, for example setting background colors and fonts, and laying out our content in multiple columns. 
+JavaScript: Scripting language that enables you to create dynamically updating content, control multimedia, animate images, etc. 
+*/
+
+// comment version 1
+/* comment version 2 */
+
+//In the Debugger view panel, you will see a column of numbers indicating the line numbers for each line in the JavaScript code file. This column is called the 'gutter'.
