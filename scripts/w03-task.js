ad9e07880b75119f13fbcc856f8e99494f913ea0
Juan-of-many <fiery.fynx@gmail.com>
2024-04-06 20:07:36 -0800

    w06 project

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -11,7 +11,7 @@ function addNumbers () {
     let addNumber1 = Number(document.querySelector("#add1").value);
     let addNumber2 = Number(document.querySelector("#add2").value); 
     
-    document.querySelector("sum").value = add(addNumber1, addNumber2);
+    document.querySelector("#sum").value = add(addNumber1, addNumber2);
 }
 
 document.querySelector("#addNumbers").addEventListener("click", addNumbers);
---
+++ scripts/w06-task.js
@@ -0,0 +1,120 @@
+
+/* Warning, what follows is a list from wikipedia of english swear words.
+Scroll past to the rest of the code if you so desire. */
+var bad_words = [
+    `arse`,
+    `arsehead`,
+    `arsehole`,
+    `ass`,
+    `asshole`,
+    `bastard`,
+    `bitch`,
+    `bloody`,
+    `bollocks`,
+    `brotherfucker`,
+    `bugger`,
+    `bullshit`,
+    `child-fucker`,
+    `Christ on a bike`,
+    `Christ on a cracker`,
+    `cock`,
+    `cocksucker`,
+    `crap`,
+    `cunt`,
+    `damn`,
+    `damnit`,
+    `damn it`,
+    `dick`,
+    `dickhead`,
+    `dyke`,
+    `fatherfucker`,
+    `frigger`,
+    `fuck`,
+    `fucker`,
+    `goddamn`,
+    `godsdamn`,
+    `hell`,
+    `holy shit`,
+    `horseshit`,
+    `Jesus Christ`,
+    `Jesus fuck`,
+    `Jesus H. Christ`,
+    `Jesus Harold Christ`,
+    `Jesus, Mary and Joseph`,
+    `Jesus Mary and Joseph`,
+    `Jesus and Mary and Joseph`,
+    `kike`,
+    `motherfucker`,
+    `nigga`,
+    `nigger`,
+    `nigra`,
+    `piss`,
+    `prick`,
+    `pussy`,
+    `shit`,
+    `shite`,
+    `sisterfucker`,
+    `slut`,
+    `son of a whore`,
+    `son of a bitch`,
+    `sunova`,
+    `sonuva`,
+    `sweet Jesus`,
+    `twat`,
+    `wanker`,
+    `poop`
+]
+
+
+
+
+
+
+
+
+function censor_check(event) {
+
+event.preventDefault();
+
+let user_word = document.getElementById(`cone_of_silence`)
+
+if (user_word in bad_words) {
+
+const Thes = `https://dictionaryapi.com/api/v3/references/thesaurus/json/`
+//user word here
+const Aurus =  `?key=4374a1f1-034f-4ce7-8fa5-341e429800ad`
+const ThesAurus = `${Thes}${user_word}${Aurus}`
+
+let alt_words = []
+
+const getThesaurus = async () => {
+    const response = await fetch(ThesAurus)
+    alt_words = await response.json();
+} 
+
+getThesaurus()
+let alt_word = alt_words.at(0); // <------ at is my array method
+
+const Dict = `https://dictionaryapi.com/api/v3/references/collegiate/json/`
+//alt word here
+const Ionary = `?key=45061386-1028-4cea-95fa-b7393a2cc18b`
+const DictIonary = `${Dict}${alt_word}${Ionary}`
+
+const getDictionary = async () => {
+    const Response = await fetch(DictIonary)
+    word_definition = await Response.json();
+}
+
+getDictionary()
+
+    document.querySelector(`#cone_of_silence`).innerHTML = `What you entered registers as a bad word. Before you get your mouth washed out with soap, here is an alternative.`
+    document.querySelector(`#response_to_user`).innerHTML = alt_word
+    document.querySelector(`#definition`).innerHTML = word_definition
+
+} else {
+    document.querySelector(`#response_to_user`).innerHTML = `The word you entered is not found in our list of bad words. Try another word!`
+    /* If the word is not in the dictionary of bad words, inform user their word is not found in this list of bad words and invite user to check other words. */
+}
+}
+
+div.addEventListener('click',censor_check);
